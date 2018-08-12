ef82682f3f41a48b9666de863f9124b301ab8488
Alexander Omelchuk <cattivole@gmail.com>
2018-08-12 15:57:44 +0300

    Initial Commit

---
+++ Roll Dice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roll Dice
+//
+//  Created by Alexander Omelchuk on 12.08.2018.
+//  Copyright © 2018 Alexander Omelchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
