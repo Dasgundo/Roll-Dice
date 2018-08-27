7c843b74e89025a8bb7108ec03918a99901c5461
Alexander <cattivole@icloud.com>
2018-08-27 13:36:16 +0300

    New working version

--- Roll Dice/ViewController.swift
+++ Roll Dice/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var leftImage: UIImageView!
+    
+    @IBOutlet weak var rightImage: UIImageView!
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        rollDice()
+    }
+    
+    func rollDice(){
+        
+        let One = arc4random_uniform(6) + 1
+        
+        let Two = arc4random_uniform(6) + 1
+        
+        leftImage.image = UIImage(named: "Dice\(One)")
+        
+        rightImage.image = UIImage(named: "Dice\(Two)")
+        
+        display.text = "Сумма: \(One + Two)"
+        
+        AudioServicesPlaySystemSound(SystemSoundID(1104))
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        display.text = "Привет игрок!"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
